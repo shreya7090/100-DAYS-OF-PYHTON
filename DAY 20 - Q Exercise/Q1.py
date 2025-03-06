9c26bae827c6fba1a90226fb71ad6a4129c7f382
shreya7090 <Your GitHub Email>
2025-03-06 18:50:17 +0530

    Code Exercise

---
+++ DAY 20 - Q Exercise/Q1.py
@@ -0,0 +1,10 @@
+class Dog:
+    def __init__(self):
+        self.temperament = "loyal"
+ 
+    def bark(self):
+        print("Woof, woof!")
+
+class Labrador(Dog):
+    def __init__(self):
+        self.temperament = "friendly"
